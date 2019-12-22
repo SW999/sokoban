f343debccdd4e7d95e7287c292d0e9db9666d8ff
Siarhei <swarog2002@mail.ru>
2019-12-22 19:19:11 +0300

    Remove event listeners if level is complete

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function (e) {
+document.addEventListener('DOMContentLoaded', function () {
   var mapBlocks, // массив элементов игрового поля по классу
     world = {
       map: [ // 1 - возможно перемещение, 2 - выигрышное положение
@@ -28,7 +28,11 @@ document.addEventListener('DOMContentLoaded', function (e) {
     stepSize, // ширина одного элемента поля с отступами = шаг персонажа
     winComb = [], // массив с координатами "выигрышных" положений
     tmpStatus = {}, // сосояние поля на предыдущем ходе
-    manager = {};
+    boss = {},
+    isTouchDevice = document.documentElement.classList.contains('touch'),
+    $undoBtn = document.getElementById('undoBtn'),
+    $map = document.querySelector('.map'),
+    $start = document.getElementById('start-game');
 
   function findPos(obj) {
     if (obj.offsetParent) {
@@ -70,13 +74,13 @@ document.addEventListener('DOMContentLoaded', function (e) {
         }
         if (personStatus === 2) {
           block = document.createElement('div');
-          block.id = "manager";
-          block.className = "person manager";
+          block.id = "boss";
+          block.className = "person boss";
           block.style.top = findPos(mapBlocks[blockPos])[1] + 'px';
           block.style.left = findPos(mapBlocks[blockPos])[0] + 'px';
           fragment.appendChild(block);
           world.person[i][j] = 0;
-          manager = {
+          boss = {
             el: block,
             pos: [i, j]
           }
@@ -84,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
       });
     });
     document.querySelector('.map').appendChild(fragment);
-    return manager;
+    return boss;
   }
 
   function checkGameStatus() {
@@ -97,27 +101,29 @@ document.addEventListener('DOMContentLoaded', function (e) {
   }
 
   function levelComplete() {
-    console.log('You win!'); // TODO: добавить сообщение и возможность перехода на новый уровень
-    var kenny = document.querySelector('#slacker2');
-    var angel = document.querySelector('.angel');
+    var kenny = document.getElementById('slacker2'),
+      angel = document.querySelector('.angel');
+
     angel.style.top = kenny.offsetTop + 'px';
     angel.style.left = kenny.offsetLeft + 'px';
-
     kenny.classList.add('dead');
-    angel.classList.add('show');
 
+    angel.classList.add('show');
     document.querySelector('audio').play();
+
+    console.log('You win!'); // TODO: добавить сообщение и возможность перехода на новый уровень
+    stopEvents();
   }
 
   /* сосояние поля на предыдущем ходе */
   function prevStatus(obj) {
     return tmpStatus = {
-      lastManager: {
-        left: manager.el.style.left,
-        top: manager.el.style.top,
-        pos: manager.pos.slice()
+      prevBoss: {
+        left: boss.el.style.left,
+        top: boss.el.style.top,
+        pos: boss.pos.slice()
       },
-      lastElem: obj === null ? false : {
+      prevElem: obj === null ? false : {
         el: obj.id,
         left: obj.id.style.left,
         top: obj.id.style.top,
@@ -132,143 +138,135 @@ document.addEventListener('DOMContentLoaded', function (e) {
     checkGameStatus();
   }
 
-  function stepLeft(e) {
-    e.stopPropagation();
-
+  function stepLeft() {
     var newPos,
-      y = manager.pos[0],
-      x = manager.pos[1] - 1;
+      y = boss.pos[0],
+      x = boss.pos[1] - 1;
 
     if (world.map[y][x]) {
       var leftPos = world.person[y][x];
-      newPos = parseInt(manager.el.style.left, 10) - stepSize + 'px';
+      newPos = parseInt(boss.el.style.left, 10) - stepSize + 'px';
 
       if (leftPos && world.map[y][x - 1] && !world.person[y][x - 1]) {
         prevStatus({ id: leftPos, prevPos: [y, x], curPos: [y, x - 1] });
         leftPos.style.left = (parseInt(leftPos.style.left, 10) - stepSize) + 'px';
         world.person[y][x - 1] = leftPos;
-        manager.pos[1] = x;
-        manager.el.style.left = newPos;
+        boss.pos[1] = x;
+        boss.el.style.left = newPos;
         finalizeStep(y, x);
       } else if (!leftPos) {
         prevStatus(null);
-        manager.pos[1] = x;
-        manager.el.style.left = newPos;
+        boss.pos[1] = x;
+        boss.el.style.left = newPos;
       }
     }
   }
 
-  function stepUp(e) {
-    e.stopPropagation();
-
+  function stepUp() {
     var newPos,
-      y = manager.pos[0] - 1,
-      x = manager.pos[1];
+      y = boss.pos[0] - 1,
+      x = boss.pos[1];
 
-    if (manager.pos[0] > 0 && world.map[y][x]) {
+    if (boss.pos[0] > 0 && world.map[y][x]) {
       var topPos = world.person[y][x];
-      newPos = parseInt(manager.el.style.top, 10) - stepSize + 'px';
+      newPos = parseInt(boss.el.style.top, 10) - stepSize + 'px';
 
       if ((y + 1) > 1 && topPos && world.map[y - 1][x] && !world.person[y - 1][x]) {
         prevStatus({ id: topPos, prevPos: [y, x], curPos: [y - 1, x] });
         topPos.style.top = (parseInt(topPos.style.top, 10) - stepSize) + 'px';
         world.person[y - 1][x] = topPos;
-        manager.pos[0] = y;
-        manager.el.style.top = newPos;
+        boss.pos[0] = y;
+        boss.el.style.top = newPos;
         finalizeStep(y, x);
       } else if (!topPos) {
         prevStatus(null);
-        manager.pos[0] = y;
-        manager.el.style.top = newPos;
+        boss.pos[0] = y;
+        boss.el.style.top = newPos;
       }
     }
   }
 
-  function stepRight(e) {
-    e.stopPropagation();
-
+  function stepRight() {
     var newPos,
-    y = manager.pos[0],
-    x = manager.pos[1] + 1;
+    y = boss.pos[0],
+    x = boss.pos[1] + 1;
 
     if (world.map[y][x]) {
       var rightPos = world.person[y][x];
-      newPos = parseInt(manager.el.style.left, 10) + stepSize + 'px';
+      newPos = parseInt(boss.el.style.left, 10) + stepSize + 'px';
 
       if (rightPos && world.map[y][x + 1] && !world.person[y][x + 1]) {
         prevStatus({ id: rightPos, prevPos: [y, x], curPos: [y, x + 1] });
         rightPos.style.left = (parseInt(rightPos.style.left, 10) + stepSize) + 'px';
         world.person[y][x + 1] = rightPos;
-        manager.pos[1] = x;
-        manager.el.style.left = newPos;
+        boss.pos[1] = x;
+        boss.el.style.left = newPos;
         finalizeStep(y, x);
       } else if (!rightPos) {
         prevStatus(null);
-        manager.el.style.left = newPos;
-        manager.pos[1] = x;
+        boss.el.style.left = newPos;
+        boss.pos[1] = x;
       }
     }
   }
 
-  function stepDown(e) {
-    e.stopPropagation();
-
+  function stepDown() {
     var newPos,
-      y = manager.pos[0] + 1,
-      x = manager.pos[1];
+      y = boss.pos[0] + 1,
+      x = boss.pos[1];
 
     if ((y - 1) < (mapHeight - 1) && world.map[y][x]) {
       var bottomPos = world.person[y][x];
-      newPos = parseInt(manager.el.style.top, 10) + stepSize + 'px';
+      newPos = parseInt(boss.el.style.top, 10) + stepSize + 'px';
 
       if ((y - 1) < (mapHeight - 2) && bottomPos && world.map[y + 1][x] && !world.person[y + 1][x]) {
         prevStatus({ id: bottomPos, prevPos: [y, x], curPos: [y + 1, x] });
         bottomPos.style.top = (parseInt(bottomPos.style.top, 10) + stepSize) + 'px';
         world.person[y + 1][x] = bottomPos;
-        manager.pos[0] = y;
-        manager.el.style.top = newPos;
+        boss.pos[0] = y;
+        boss.el.style.top = newPos;
         finalizeStep(y, x);
       } else if (!bottomPos) {
         prevStatus(null);
-        manager.pos[0] = y;
-        manager.el.style.top = newPos;
+        boss.pos[0] = y;
+        boss.el.style.top = newPos;
       }
     }
   }
 
   function undo() {
-    manager.pos = tmpStatus.lastManager.pos.slice(); // Возвращаем "начальника" на предыдущее место
-    manager.el.style.left = tmpStatus.lastManager.left;
-    manager.el.style.top = tmpStatus.lastManager.top;
+    boss.pos = tmpStatus.prevBoss.pos.slice(); // Возвращаем "начальника" на предыдущее место
+    boss.el.style.left = tmpStatus.prevBoss.left;
+    boss.el.style.top = tmpStatus.prevBoss.top;
 
-    if (tmpStatus.lastElem) { // Если перемещался персонаж, то возвращаем его на предыдущее место
-      var el = tmpStatus.lastElem.el;
+    if (tmpStatus.prevElem) { // Если перемещался персонаж, то возвращаем его на предыдущее место
+      var el = tmpStatus.prevElem.el;
 
-      el.style.left = tmpStatus.lastElem.left;
-      el.style.top = tmpStatus.lastElem.top;
-      world.person[tmpStatus.lastElem.pos1[0]][tmpStatus.lastElem.pos1[1]] = el;
-      world.person[tmpStatus.lastElem.pos2[0]][tmpStatus.lastElem.pos2[1]] = 0;
+      el.style.left = tmpStatus.prevElem.left;
+      el.style.top = tmpStatus.prevElem.top;
+      world.person[tmpStatus.prevElem.pos1[0]][tmpStatus.prevElem.pos1[1]] = el;
+      world.person[tmpStatus.prevElem.pos2[0]][tmpStatus.prevElem.pos2[1]] = 0;
     }
   }
 
-  function moveManager(e) {
+  function moveBoss(e) {
     var code = e.which;
 
     switch (code) {
       case 37: //left
-        stepLeft(e);
+        stepLeft();
         break;
 
       case 38: //top
-        stepUp(e);
+        stepUp();
         break;
 
       case 39: //right
-        stepRight(e);
+        stepRight();
         break;
 
       case 40: // down
-        stepDown(e);
+        stepDown();
         break;
 
       case 32: // undo клавиша "space"
@@ -284,9 +282,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
     // Строим игровое поле.
     var fragment = document.createDocumentFragment(),
       i = 256,
-      angelBlock = document.createElement('div'),
-      $map = document.querySelector('.map'),
-      $start = document.getElementById('start-game');
+      angelBlock = document.createElement('div');
 
     angelBlock.className = 'angel';
     fragment.appendChild(angelBlock);
@@ -302,21 +298,32 @@ document.addEventListener('DOMContentLoaded', function (e) {
     stepSize = document.querySelector('.map-item').clientWidth + 3;
 
     createMap();
-    if (document.documentElement.classList.contains('touch')) { // event listeners for mobile devices
+    if (isTouchDevice) { // event listeners for touch devices
       document.getElementById('controls-definition').textContent = 'swipe gestures';
       document.getElementById('undo-definition').textContent = 'undo button';
-      $map.addEventListener('swipeLeft', stepLeft, false);
-      $map.addEventListener('swipeUp', stepUp, false);
-      $map.addEventListener('swipeRight', stepRight, false);
-      $map.addEventListener('swipeDown', stepDown, false);
-      $start.addEventListener('click', function (e) {
-        e.preventDefault();
+      $map.addEventListener('swipeLeft', stepLeft);
+      $map.addEventListener('swipeUp', stepUp);
+      $map.addEventListener('swipeRight', stepRight);
+      $map.addEventListener('swipeDown', stepDown);
+      $start.addEventListener('click', function () {
         document.getElementById('dashboard').classList.add('hidden');
-      }, false);
-      document.getElementById('undoBtn').addEventListener('click', undo, false);
+      });
+      $undoBtn.addEventListener('click', undo);
     } else {
-      document.addEventListener('keyup', moveManager, false);
+      document.addEventListener('keyup', moveBoss);
     }
   })();
+
+  function stopEvents() {
+    if (isTouchDevice) {
+      $map.removeEventListener('swipeLeft', stepLeft);
+      $map.removeEventListener('swipeUp', stepUp);
+      $map.removeEventListener('swipeRight', stepRight);
+      $map.removeEventListener('swipeDown', stepDown);
+      $undoBtn.removeEventListener('click', undo);
+    } else {
+      document.removeEventListener('keyup', moveBoss);
+    }
+  }
 
 });
